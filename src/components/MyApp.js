8ae3651513a3b37ff5db8314004fe66bb336e130
saumya <2saumyaray@gmail.com>
2016-05-02 20:56:06 +0530

    code cleanup

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -1,6 +1,16 @@
 // Root Component
-import React from 'react'
+/*
+// ref : https://facebook.github.io/react/docs/reusable-components.html#stateless-functions
+Type 1:
+function App(props) {
+  return <div>Hello {props.name}</div>;
+}
 
+Type 2:
+const App = (props) => <div>Hello {props.name}</div>;
+*/
+// Component as pure function
+import React from 'react'
 const App = () => (
   <div>
     Welcome to React-Redux-Thunk 101
