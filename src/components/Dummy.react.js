5493560758dd6e2b9bc126832443b0273432429b
saumya <2saumyaray@gmail.com>
2016-05-03 21:01:25 +0530

    wip components

--- src/components/Dummy.react.js
+++ src/components/Dummy.react.js
@@ -1,36 +1,38 @@
 //
-import React from 'react'
+import React, {Component} from 'react'
 import { connect } from 'react-redux'
 //import { addTodo } from '../actions'
 
-let Dummy = ({ dispatch }) => {
-  var uName,uPassword ;
-  var that = this ;
-  return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault()
+class Dummy extends Component{
+  render(){
+    var uName,uPassword ;
+    var that = this ;
+    return (
+      <div>
+        <form onSubmit={e => {
+          e.preventDefault()
 
-        console.group("Dummy : '%s'", 'Dummy');
-        console.log('Dummy');
-        console.log(dispatch);
-        console.log('Dummy : Submit : uName', uName.value.trim());
-        console.log('Dummy : Submit : uPassword', uPassword.value.trim());
-        console.groupEnd();
+          console.group("Dummy : '%s'", 'Dummy');
+          console.log('Dummy');
+          console.log(dispatch); //Uncaught ReferenceError: dispatch is not defined
+          console.log('Dummy : Submit : uName', uName.value.trim());
+          console.log('Dummy : Submit : uPassword', uPassword.value.trim());
+          console.groupEnd();
 
-        if (!uName.value.trim() || !uPassword.value.trim() ) {
-          return
-        }
+          if (!uName.value.trim() || !uPassword.value.trim() ) {
+            return
+          }
 
-        //dispatch(addTodo(uName.value,uPassword.value))
-        //uName.value = uPassword.value = ''
-      }}>
-        <input ref={node1 => { uName = node1 }} />
-        <input ref={node2 => { uPassword = node2 }} />
-        <button type="submit"> Dummy </button>
-      </form>
-    </div>
-  )
+          //dispatch(addTodo(uName.value,uPassword.value))
+          //uName.value = uPassword.value = ''
+        }}>
+          <input ref={node1 => { uName = node1 }} />
+          <input ref={node2 => { uPassword = node2 }} />
+          <button type="submit"> Dummy </button>
+        </form>
+      </div>
+    )
+  }
 }
 
 Dummy = connect()(Dummy)
