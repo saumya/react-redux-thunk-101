13364214a334ba2ead90baec90751b7729cb0946
saumya <2saumyaray@gmail.com>
2016-05-03 20:32:25 +0530

    wip component

--- src/components/Login.react.js
+++ src/components/Login.react.js
@@ -1,32 +1,37 @@
-//
-'use strict';
-import React, {Component} from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+//import { addTodo } from '../actions'
 
-class Login extends Component{
-  constructor(props) {
-    super(props)
-  }
-  render(){
-    var uName,uPassword;
-    return (
-      <div>
-        <form onSubmit={e => {
-          e.preventDefault()
-          console.log('Login : Submit : uName', uName.value.trim());
-          console.log('Login : Submit : uPassword', uPassword.value.trim());
-          if (!uName.value.trim() || !uPassword.value.trim()) {
-            return
-          }
-          //dispatch(addTodo(input.value))
-          //input.value = ''
-        }}>
-          <input ref={node => { uName = node }} defaultValue={"user name"}/>
-          <input ref={node => { uPassword = node }} defaultValue={"password"}/>
-          <button type="submit"> Login </button>
-        </form>
-      </div>
-    )
-  }
+let Login = ({ dispatch }) => {
+  let input1
+  let input2
+
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+
+        console.group("Add Contact : '%s'", 'AddContact');
+        console.log('Add Contact');
+        console.log(dispatch);
+        console.log('input1', input1, input1.value);
+        console.log('input2', input2, input2.value);
+        console.groupEnd();
+
+        if (!input1.value.trim() || !input2.value.trim() ) {
+          return
+        }
+
+        //dispatch(addTodo(input1.value,input2.value))
+        //input1.value = input2.value = ''
+      }}>
+        <input ref={node1 => { input1 = node1 }} />
+        <input ref={node2 => { input2 = node2 }} />
+        <button type="submit"> Add Contact </button>
+      </form>
+    </div>
+  )
 }
+Login = connect()(Login)
 
 export default Login
